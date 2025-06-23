d43c852439c5ae145b49a84c147ccd8bf9c2e508
Andromeda <stroev.andrew@gmail.com>
2025-06-24 00:25:34 +0100

    Fix NixOS shell.nix at long last

--- shell.nix
+++ shell.nix
@@ -1,22 +1,32 @@
-{ pkgs ? import <nixpkgs> {} }:
+{pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  packages = with pkgs; [
-    mermaid-cli
-    presenterm
-    typst
-
-    (python312.withPackages (pypkgs: with pypkgs; [
-      aiohttp
-      fastapi
-      lark
-      numpy
-      pyaudio
-      python-rtmidi
-      soundfile
-      textual
-      typer
-      uvicorn
-    ]))
+let
+  python = pkgs.python312;
+  buildInputs = with pkgs; [
+    stdenv.cc.cc
+    portaudio
   ];
-}
+in
+  pkgs.mkShell {
+    packages = [
+      (python.withPackages (pypkgs: with pypkgs; [
+        uv
+      ]))
+    ];
+
+#    shellHook = ''
+#      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}
+#      export UV_PYTHON_DOWNLOADS=never
+#      export UV_USE_MANAGED_PYTHON=0
+#      export UV_PYTHON=${pkgs.python312}/bin/python3.12
+#    '';
+
+    shellHook = ''
+      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath buildInputs}
+      export UV_PYTHON_DOWNLOADS=never
+      export UV_USE_MANAGED_PYTHON=0
+
+      export CFLAGS="-I${pkgs.portaudio}/include"
+      export LDFLAGS="-L${pkgs.portaudio}/lib"
+    '';
+  }
